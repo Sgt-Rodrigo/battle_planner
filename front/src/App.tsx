862164600150058434754388791926eed096f7de
Sgt-Rodrigo <rod_raven@hotmail.com>
2024-04-25 16:59:09 -0300

    -creates vite-react with Typescript proyect, cleans up scafolding -tweaks tsconfig and eslintrc -creates Home and Navbar components -installs react-bootstrap and sass -sets up basic bootstrap navbar, global styles and scss modules.

---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss'; //w global styles
+import Home from './views/Home/Home'; 
+
+function App() {
+
+  return (
+    <>     
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ front/src/views/Home/Home.tsx
@@ -0,0 +1,12 @@
+import MainNavbar from '../../components/MainNavbar/MainNavbar';
+import home from './Home.module.scss';
+
+function Home() {
+  return (
+    <>
+        <MainNavbar/>
+    </>
+  )
+}
+
+export default Home
